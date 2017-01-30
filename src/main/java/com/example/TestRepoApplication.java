9c4bddd4ca87519d31233a37c3ebd45696eef09e
vjindalaurea <vikas.jindal@aurea.com>
2017-01-30 10:08:56 +0530

    Intial Code

---
+++ src/main/java/com/example/TestRepoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepoApplication.class, args);
+	}
+}
