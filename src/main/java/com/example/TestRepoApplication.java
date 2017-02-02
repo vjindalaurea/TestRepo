5130fe4e69077e8c00b88c0549754f905b2930a0
vjindalaurea <vikas.jindal@aurea.com>
2017-02-02 12:19:02 +0530

    Update TestRepoApplication.java
    C1

--- src/main/java/com/example/TestRepoApplication.java
+++ src/main/java/com/example/TestRepoApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestRepoApplication {
+	
+	
+	  public void display() {
+	    System.out.println("Change 1");
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestRepoApplication.class, args);
