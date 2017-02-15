e2d6c7b1a73d22224c14b803b279cd7e78f415b1
vjindalaurea <vikas.jindal@aurea.com>
2017-02-15 10:45:58 +0530

    Adding TestCase

---
+++ src/test/java/com/example/service/TestServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+import org.junit.Test;
+
+public class TestServiceTest {
+	
+	@Test
+	public void testtestMethod1(){
+		TestService service = new TestService();
+		service.testMethod1();
+		service.testMethod2();
+	}
+
+}
