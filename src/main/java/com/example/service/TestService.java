61b1123f1d09c52d1350742a1deb01773b5f8113
vjindalaurea <vikas.jindal@aurea.com>
2017-02-15 09:06:03 +0530

    Modifying TestService

--- src/main/java/com/example/service/TestService.java
+++ src/main/java/com/example/service/TestService.java
@@ -5,5 +5,10 @@ public class TestService {
 	public void testMethod1(){
 		System.out.println("This is test Method1 ");
 	}
+	
+	public void testMethod2(){
+		int a = 4/2;
+		System.out.println("This is test Method1 "+a);
+	}
 
 }
---
+++ src/main/java/com/example/service/TestService2.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+public class TestService2 {
+	
+	public void testmyMethod1(){
+		System.out.println("1+2 testmyMethod1" );
+	}
+
+}
